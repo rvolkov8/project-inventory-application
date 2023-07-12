8be479a0f1ebd4545f83166a6ff830ec0c0e0a39
Rostyslav <volkov.rostyslav.8@gmail.com>
2023-07-12 21:47:06 +0300

    Implement contollers, routes, views and css for the log in form

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -14,8 +14,44 @@ exports.getSignUpForm = asyncHandler(async (req, res, next) => {
   }
   res.render('signUpForm', { categories: categories });
 });
-
 //POST sign up form
+exports.postSignUpForm = asyncHandler(async (req, res, next) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    return next({ message: result.array() });
+  }
+  const body = req.body;
+  if (body.password !== body.confirmPassword) {
+    return next({ message: 'The password confirmation does not match.' });
+  }
+
+  bcrypt.genSalt(10, async (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    const hashedPassword = await bcrypt.hash(body.password, salt);
+
+    const newUser = new User({
+      firstName: body.firstName,
+      lastName: body.lastName,
+      username: body.username,
+      password: hashedPassword,
+      isAdmin: false,
+    });
+    await newUser.save();
+    res.redirect('/log-in');
+  });
+});
+
+//GET sign up form
+exports.getLogInForm = asyncHandler(async (req, res, next) => {
+  const categories = await Category.find().sort('name').exec();
+  if (req.user) {
+    res.redirect('/profile');
+  }
+  res.render('logInForm', { categories: categories });
+});
+//POST log in form
 exports.postSignUpForm = asyncHandler(async (req, res, next) => {
   const result = validationResult(req);
   if (!result.isEmpty()) {
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const profileController = require('../controllers/profileController');
 const { body } = require('express-validator');
@@ -28,7 +29,26 @@ router.post(
     .notEmpty()
     .withMessage('Username field is empty.')
     .escape(),
+  body('password').escape(),
+  body('confirmPassword').escape(),
   profileController.postSignUpForm
+);
+
+//GET log in form
+router.get('/log-in', profileController.getLogInForm);
+//POST log in form
+router.post(
+  '/log-in',
+  body('username')
+    .trim()
+    .notEmpty()
+    .withMessage('Username field is empty.')
+    .escape(),
+  body('password').escape(),
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/log-in',
+  })
 );
 
 module.exports = router;
